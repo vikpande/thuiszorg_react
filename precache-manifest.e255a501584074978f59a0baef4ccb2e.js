189a60350a13572a86291d4cff9406e12f26ef91
vikpande <vikpande@gmail.com>
2019-03-19 09:53:21 +0100

    Updates

---
+++ precache-manifest.e255a501584074978f59a0baef4ccb2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aeafcbf8a211084ec8bd",
+    "url": "/thuiszorg_react/static/css/main.1459ddc1.chunk.css"
+  },
+  {
+    "revision": "aeafcbf8a211084ec8bd",
+    "url": "/thuiszorg_react/static/js/main.aeafcbf8.chunk.js"
+  },
+  {
+    "revision": "2684e75bd9d710405efe",
+    "url": "/thuiszorg_react/static/js/runtime~main.2684e75b.js"
+  },
+  {
+    "revision": "ea63894964c0eab251a4",
+    "url": "/thuiszorg_react/static/js/2.ea638949.chunk.js"
+  },
+  {
+    "revision": "a60cb0b3a33e9dd25e19727a8b05216f",
+    "url": "/thuiszorg_react/index.html"
+  }
+];
