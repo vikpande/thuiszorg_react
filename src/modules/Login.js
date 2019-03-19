dee97ee0ef302393c8322c7ec2dca868986b62b6
Vikas Pandey <vikpande@gmail.com>
2019-03-19 14:02:52 +0100

    commented out "required" validation for e-mail login page

--- src/modules/Login.js
+++ src/modules/Login.js
@@ -1,40 +1,40 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import LogoLarge from "../components/LogoLarge";
-import Block from "../components/Block";
-import LoginForm from "../components/LoginForm";
-import Input from "../components/Input";
-import HorizontalGroup from "../components/HorizontalGroup";
-import Button from "../components/Button";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import LogoLarge from '../components/LogoLarge';
+import Block from '../components/Block';
+import LoginForm from '../components/LoginForm';
+import Input from '../components/Input';
+import HorizontalGroup from '../components/HorizontalGroup';
+import Button from '../components/Button';
 
 class Login extends Component {
-
-  render() {
-    return (
-      <React.Fragment>
-        <LogoLarge />
-        <Block>
-          <h1>Inloggen</h1>
-          <LoginForm action="/dashboard">
-            <Input
-              type="email"
-              required
-              placeholder="E-mailadres"
-            /><Input
-              type="password"
-              required
-              placeholder="Wachtwoord"
-            />
-            <HorizontalGroup>
-              <Link to="/forgot-password">Wachtwoord vergeten?</Link>
-              <Button label="Inloggen" type="submit" />
-            </HorizontalGroup>
-          </LoginForm>
-          <p>Als je nog geen Babythuiszorg account hebt kan je informatie aanvragen bij je manager.</p>
-        </Block>
-      </React.Fragment>
-    );
-  }
+	render() {
+		return (
+			<React.Fragment>
+				<LogoLarge />
+				<Block>
+					<h1>Inloggen</h1>
+					<LoginForm action="/dashboard">
+						<Input
+							type="email"
+							// required
+							placeholder="E-mailadres"
+						/>
+						<Input
+							type="password"
+							// required
+							placeholder="Wachtwoord"
+						/>
+						<HorizontalGroup>
+							<Link to="/forgot-password">Wachtwoord vergeten?</Link>
+							<Button label="Inloggen" type="submit" />
+						</HorizontalGroup>
+					</LoginForm>
+					<p>Als je nog geen Babythuiszorg account hebt kan je informatie aanvragen bij je manager.</p>
+				</Block>
+			</React.Fragment>
+		);
+	}
 }
 
 export default Login;
